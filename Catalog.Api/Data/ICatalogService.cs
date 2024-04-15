cd7b12dc4b57967417455124a34cd7171facbf20
HectorArandaImproving <Hector.Aranda@improving.com>
2024-04-14 22:11:49 -0600

    Add project files.

---
+++ Catalog.Api/Data/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public interface ICatalogService
+    {
+        // DBSet<>
+        IMongoCollection<Product> Products { get; set; }
+    }
+}
